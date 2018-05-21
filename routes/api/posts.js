96bd3992469db751e0c51f638b9bacc7f3d01359
Robert Marsh <nhe6dc@gmail.com>
2018-05-21 12:11:06 -0400

    delete and get post routes added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,10 +4,26 @@ const mongoose = require("mongoose");
 const passport = require("passport");
 
 const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
 
 const validatePostInput = require("../../validation/post");
 
 router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
+
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ nopostfound: "No posts found" }));
+});
+
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(404).json({ nopostfound: "No post found with that id" })
+    );
+});
 
 router.post(
   "/",
@@ -27,6 +43,27 @@ router.post(
     });
 
     newPost.save().then(post => res.json(post));
+  }
+);
+
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.body.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          // check for post owner
+          if (post.user.toString() !== req.user.id) {
+            return res
+              .status(401)
+              .json({ notauthorized: "User not authorized" });
+          }
+
+          post.remove().then(() => res.json({ successs: true }));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "no post found" }));
+    });
   }
 );
 
