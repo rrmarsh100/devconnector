c17a09e4d615f26e49cd1e1c38278897ebdf4c61
Robert Marsh <nhe6dc@gmail.com>
2018-05-24 15:27:23 -0400

    login and logout

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -7,7 +7,7 @@ import { GET_ERRORS, SET_CURRENT_USER } from "./Types";
 // Register User
 export const registerUser = (userData, history) => dispatch => {
   axios
-    .post("api/users/register", userData)
+    .post("/api/users/register", userData)
     .then(res => history.push("/login"))
     .catch(err =>
       dispatch({
@@ -17,20 +17,20 @@ export const registerUser = (userData, history) => dispatch => {
     );
 };
 
-//login get user token
+// Login - Get User Token
 export const loginUser = userData => dispatch => {
   axios
     .post("/api/users/login", userData)
     .then(res => {
-      //save to local storage
+      // Save to localStorage
       const { token } = res.data;
-      //set token to local storage
+      // Set token to ls
       localStorage.setItem("jwtToken", token);
-      //set to auth header
+      // Set token to Auth header
       setAuthToken(token);
       // Decode token to get user data
       const decoded = jwt_decode(token);
-      // Set CUrrent User
+      // Set current user
       dispatch(setCurrentUser(decoded));
     })
     .catch(err =>
@@ -41,10 +41,20 @@ export const loginUser = userData => dispatch => {
     );
 };
 
-//Set logged in user
+// Set logged in user
 export const setCurrentUser = decoded => {
   return {
     type: SET_CURRENT_USER,
     payload: decoded
   };
+};
+
+// Log user out
+export const logoutUser = () => dispatch => {
+  // Remove token from localStorage
+  localStorage.removeItem("jwtToken");
+  // Remove auth header for future requests
+  setAuthToken(false);
+  // Set current user to {} which will set isAuthenticated to false
+  dispatch(setCurrentUser({}));
 };
