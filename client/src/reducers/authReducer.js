6a32428596d944a6f0efb7c7ff2edcfa01cd3b94
Robert Marsh <nhe6dc@gmail.com>
2018-05-24 10:30:28 -0400

    action workflow example

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,9 +1,16 @@
+import { TEST_DISPATCH } from "../actions/Types";
+
 const initialState = {
   isAuthenticated: false,
   user: {}
 };
 export default function(state = initialState, action) {
-  switch (action) {
+  switch (action.type) {
+    case TEST_DISPATCH:
+      return {
+        ...state,
+        user: action.payload
+      };
     default:
       return state;
   }
