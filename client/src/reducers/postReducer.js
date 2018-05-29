ed64897278e273937b248dd4e57ccf4e095bba1d
Robert Marsh <nhe6dc@gmail.com>
2018-05-29 15:48:44 -0400

    Finished first version

--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -4,7 +4,7 @@ import {
   GET_POST,
   DELETE_POST,
   POST_LOADING
-} from "../actions/types";
+} from "../actions/Types";
 
 const initialState = {
   posts: [],
@@ -14,6 +14,33 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case POST_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    case GET_POSTS:
+      return {
+        ...state,
+        posts: action.payload,
+        loading: false
+      };
+    case GET_POST:
+      return {
+        ...state,
+        post: action.payload,
+        loading: false
+      };
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [action.payload, ...state.posts]
+      };
+    case DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter(post => post._id !== action.payload)
+      };
     default:
       return state;
   }
